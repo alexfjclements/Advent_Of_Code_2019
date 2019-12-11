fca2e23550567f3125cb23200ebee9fca9ca8650
Alexander Clements <alexfjclements@gmail.com>
2019-12-11 14:54:01 +0000

    dec 2 part 2 in progress- dictionary length change bug

--- dec_2/dec_2_calc.py
+++ dec_2/dec_2_calc.py
@@ -1,4 +1,4 @@
-def op_code_processor(input_list):
+def op_code_processor(input_list, reverse_value=False):
     new_dict = {}
     counter = 0
     output = input_list
@@ -13,7 +13,10 @@ def op_code_processor(input_list):
 
         if key % 4 == 0:
             if value in operational_values:
-                new_dict[new_dict[key + 3]] = value_analysis(key, value, new_dict)
+                if reverse_value is True:
+                    new_dict[new_dict[key + 3]] = reverse_value_analysis(key, value, new_dict)
+                else:
+                    new_dict[new_dict[key + 3]] = value_analysis(key, value, new_dict)
             elif value == termination_value:
                 new_dict[key] = "PROGRAM TERMINATED"
                 final_values = list(new_dict.values())
@@ -32,7 +35,42 @@ def value_analysis(key, value, new_dict):
         new_value = new_dict[new_dict[key + 1]] + new_dict[new_dict[key + 2]]
     elif value == 2:
         new_value = new_dict[new_dict[key + 1]] * new_dict[new_dict[key + 2]]
-    elif value == 99:
-        new_value = "PROGRAM TERMINATED"
+
+    return new_value
+
+
+def reverse_program(data_input: list):
+    reversed_data_as_lists = []
+    reversed_data_list = []
+    new_dict = {}
+    counter = 0
+
+    for opcode in data_input:
+        new_dict[counter] = opcode
+        counter += 1
+
+    for key, value in new_dict.items():
+        if key % 4 == 0:
+            reversed_data_as_lists.append(data_input[key:key + 4])
+
+    reversed_data_as_lists = reversed_data_as_lists[::-1]
+    reversed_data_as_lists = reversed_data_as_lists[1:]
+
+    for element in reversed_data_as_lists:
+        for data in element:
+            reversed_data_list.append(data)
+
+    initial_code = op_code_processor(input_list=reversed_data_list, reverse_value=True)
+    reverse_engineered_first_group = initial_code[len(initial_code) - 4:]
+
+    return reverse_engineered_first_group
+
+
+def reverse_value_analysis(key, value, new_dict):
+    new_value = 0
+    if value == 1:
+        new_value = new_dict[new_dict[key + 1]] - new_dict[new_dict[key + 2]]
+    elif value == 2:
+        new_value = new_dict[new_dict[key + 1]] / new_dict[new_dict[key + 2]]
 
     return new_value
--- dec_2/dec_2_main.py
+++ dec_2/dec_2_main.py
@@ -1,7 +1,12 @@
 from dec_2.data import data, test_data
-from dec_2.dec_2_calc import op_code_processor
-from pprint import pprint
+from dec_2.dec_2_calc import op_code_processor, reverse_program
 
 
 if __name__ == "__main__":
-    print("Completed opcode:", op_code_processor(data))
+    dec_2_part_1 = op_code_processor(data)
+    print("Completed opcode:", dec_2_part_1)
+
+    target_value = 19690720
+
+    initial_set = reverse_program(dec_2_part_1)
+    print("Initial set for target value", target_value, "is", initial_set)
